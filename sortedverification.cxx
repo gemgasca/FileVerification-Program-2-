6553921e829d5ca85814a54afdaa5db9fa97ed2e
Gem Gasca <ggasca@ucdavis.edu>
2019-10-19 11:32:27 -0700

    not compiling on Clion :(

--- sortedverification.cxx
+++ sortedverification.cxx
@@ -5,6 +5,7 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <typeinfo>
 
 // the idea:
 // check to see if it is sorted
@@ -21,14 +22,16 @@ int main(int argc, char** argv) {
     if (json_file.is_open()){           // prints json object and its contents
         json_file >> jsonObject;
     }
-    std::cout << jsonObject.dump(2) << std::endl;
+//    std::cout << jsonObject.dump(2) << std::endl;
 
-    std::map<std::string, std::string> metadata;                    // gets json data
-//    metadata["array_size"] = jsonObject["metadata"]["arraySize"];
-//    metadata["num_samples"] = jsonObject["metadata"]["numSamples"];
+
+//    std::map<std::string, std::string> metadata;                    // gets json data
+//    metadata["array_size"] = *jsonObject["metadata"]["arraySize"];
+//    metadata["num_samples"] = *jsonObject["metadata"]["numSamples"];
 //    metadata["file"] = json_name;
 //    metadata["samplesWithInversions"] = " ";
 
+    std::vector<int> vector;
     for (int i = 1; i <= std::stoi("2"); i++) {      // iterates over every sample in the file, metadata["array_size")
         std::string num = std::to_string(i);
         std::string sample = "Sample";
@@ -36,14 +39,14 @@ int main(int argc, char** argv) {
         std::vector<int> vector;
         for (auto itr = jsonObject[sample].begin(); itr != jsonObject[sample].end(); itr++) {
             std::cout << *itr << std::endl;
+            vector.push_back(*itr);
         }
 
     }
 
-//    std::map<std::string, std::map< std::string, std::string>> json_output;       // returned json to be printed to terminal
-//    json_output["metadata"] = metadata;
-//
-//    json_output
+    for (auto elem: vector){
+        std::cout << "here " << elem << std::endl;
+    }
 
     return 0;
 }
