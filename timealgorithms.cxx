20a50e38e8c0cadf5ef3a400d230b63163e58266
Gem Gasca <ggasca@ucdavis.edu>
2019-10-29 18:09:41 -0700

    exec #1 done

--- timealgorithms.cxx
+++ timealgorithms.cxx
@@ -1,9 +1,9 @@
-//int main(int argc, char** argv) {
-//
-//    // the idea:
-//    // reads in one file
-//    // sorts using different methods
-//    // runs InsertionSort, MergeSort, and QuickSort on all samples
-//    // measures various statistics, and prints these statistics to a CSV
-//
-//}
+int main(int argc, char** argv) {
+
+    // the idea:
+    // reads in one file
+    // sorts using different methods
+    // runs InsertionSort, MergeSort, and QuickSort on all samples
+    // measures various statistics, and prints these statistics to a CSV
+
+}
